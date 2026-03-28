d584b4dda09ed8e276f8f8378b5be59a4e1bd6a4
allenpt221 <allenpt26@gmail.com>
2026-03-28 19:08:38 +0800

    frontend side

--- app/component/Navbar.tsx
+++ app/component/Navbar.tsx
@@ -1,7 +1,6 @@
 'use client'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation';
-import { useState } from 'react';
 
 import gccLogo from '@/assets/gcclogo.png';
 import Image from 'next/image';
@@ -19,17 +18,17 @@ function Navbar() {
   ];
 
   return (
-    <div className='sticky top-0 bg-[#030831] shadow-lg font-sans'>
+    <div className='sticky top-0 bg-[#030831] shadow-lg '>
       <div className='flex justify-between max-w-6xl mx-auto items-center p-4'>
-        <Link href="/" className="flex items-center gap-3 group">
+        <Link href="/" className="flex items-center gap-3">
           <div className='bg-[#00014f] rounded-full p-2'>
-            <Image src={gccLogo} alt="GCC Logo" className="h-9 w-9 rounded-full bg-primary-foreground/10" />
+            <Image src={gccLogo} alt="GCC Logo" className="h-9 w-9 rounded-full bg" />
           </div>
-            <div className="flex flex-col gap-1">
-              <span className="text-primary-foreground font-display font-bold text-lg leading-tight tracking-wide">
+            <div className="flex flex-col">
+              <span className="text-xl text-white font-semibold">
                 VaultArchve
               </span>
-              <span className="text-primary-foreground/60 text-white/70 text-[9px] font-body uppercase tracking-widest leading-none">
+              <span className="text-white/70 text-xs">
                 Guagua Community College
               </span>
             </div>
@@ -38,12 +37,18 @@ function Navbar() {
         <div className='flex gap-2'>
           {navbarItem.map((items, index) => (
             <div key={index} className=''>
-              <Link href={items.link} className={`font-semibold text-sm ${pathname === items.link ? 'bg-yellow-500 text-black' : 'hover:bg-white/10'} px-3 py-2 rounded-lg
-              ${items.name === "Sign up" ? "bg-yellow-500 text-black hover:bg-yellow-600" : ""}`}>
+              <Link href={items.link} className={`font-semibold text-sm px-3 py-2 rounded-lg ${
+                pathname === items.link
+                  ? "bg-yellow-500 text-black"
+                  : items.name === "Sign up"
+                  ? "bg-yellow-500 hover:bg-yellow-600 text-black"
+                  : "hover:bg-white/10 text-white"
+              }`}>
                 {items.name}
               </Link>
             </div>
           ))}
+
         </div>
       </div>
     </div>
