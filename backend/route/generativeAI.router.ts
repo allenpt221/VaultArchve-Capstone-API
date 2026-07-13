e1fb303df3f11ad06df52d4b4afb7c23741fcbbc
allenpt221 <allenpt26@gmail.com>
2026-07-13 21:16:21 +0800

    comment all the related for the progressive trail

--- backend/route/generativeAI.router.ts
+++ backend/route/generativeAI.router.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyToken } from '../middleware/middware';
-import { ProgressiveIntro, ProgressiveScopeLimitation, ProgressiveTrail } from '../controller/generativeAIGroq';
+// import { ProgressiveIntro, ProgressiveScopeLimitation, ProgressiveTrail } from '../controller/generativeAIGroq';
 import { RecommendedAI } from '../controller/generativeAI.controller';
 
 
@@ -9,9 +9,9 @@ const router = express.Router();
 
 router.post("/recommendation", verifyToken, RecommendedAI);
 
-router.post("/progressive", verifyToken, ProgressiveTrail);
-router.post("/progressive/introduction", verifyToken, ProgressiveIntro);
-router.post("/progressive/scopelimitation", verifyToken, ProgressiveScopeLimitation);
+// router.post("/progressive", verifyToken, ProgressiveTrail);
+// router.post("/progressive/introduction", verifyToken, ProgressiveIntro);
+// router.post("/progressive/scopelimitation", verifyToken, ProgressiveScopeLimitation);
 
 
 
